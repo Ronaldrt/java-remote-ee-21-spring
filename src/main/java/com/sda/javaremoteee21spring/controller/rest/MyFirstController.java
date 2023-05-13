4269379439bb0151c3164aaf48fc2c6caa6cd676
Ronald Ratsep <ronald.ratsep@hotmail.com>
2023-05-13 13:12:30 +0300

    try-login endpoint with GET http method

--- src/main/java/com/sda/javaremoteee21spring/controller/rest/MyFirstController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/rest/MyFirstController.java
@@ -1,17 +1,22 @@
 package com.sda.javaremoteee21spring.controller.rest;
 
 import com.sda.javaremoteee21spring.dto.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+// @Slf4j
 public class MyFirstController {
 
+    private static final Logger log = LoggerFactory.getLogger(MyFirstController.class);
     @GetMapping("/name")
     public String showMeMyName(){
         return "Ronald";
@@ -29,5 +34,13 @@ public class MyFirstController {
                 new Person("John", "Doe", 35)
         );
 
+    }
+
+    // ?param1=value1&param2=value2  - this is a generic way to pass parameters to your backend
+    // /api/try-login?login=ronald&password=12345
+    @GetMapping("/try-login")
+    public String logIn(@RequestParam(defaultValue = "JohnDoe", value = "login") String userLogin, @RequestParam(defaultValue = "pass", value = "password") String userPassword){
+        log.info("received login: [{}] and password [{}]", userLogin, userPassword);
+        return "OK";
     }
 }
