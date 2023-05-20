10e56bea154d4237ffea8b88d108471be93af2e4
Ronald Ratsep <ronald.ratsep@hotmail.com>
2023-05-20 10:25:29 +0300

    two endpoints for cars implemented - get all cars and get one car

--- README.md
+++ README.md
@@ -29,8 +29,11 @@
 - standard methods to play with those resources
 - CRUD methods: Create/Read/Update/Delete
 - Car as an example - our propositions
-  - get all cars url + Http verb: GET /all-cars
+  - get all cars (url + Http verb): GET /all-cars
   - get one particular car (url + Http verb): POST /VIN; POST /bmw
   - create new car (url + Http verb): POST /add-car; POST /add-new-car
   - delete a car (url + Http verb): DELETE /VIN; DELETE /delete-car
   - update car (url + Http verb): PATCH /VIN-patch; PATCH /update-car
+- Car entity: good practices
+  - get all cars (url + Http verb): GET /cars
+  - get one car (url + Http verb): GET /cars/{id} e.g. /cars/1024
--- src/main/java/com/sda/javaremoteee21spring/controller/rest/CarController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/rest/CarController.java
@@ -4,11 +4,14 @@ import com.sda.javaremoteee21spring.entity.Car;
 import com.sda.javaremoteee21spring.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @Slf4j
 public class CarController {
 
@@ -18,10 +21,16 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/all-cars")
+    @GetMapping("/cars")
     public List<Car> getAllCars(){
         log.info("getting all cars");
         return carService.findAllCars();
+    }
+
+    @GetMapping("/cars/{id}")
+    public Car getCarById(@PathVariable Long id){
+        log.info("Getting car by id: [{}]", id);
+        return carService.findCarById(id);
     }
 
 }
