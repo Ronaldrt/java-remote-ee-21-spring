03402fc91979f61bac83251d2c8b8746994f7a0b
Ronald Ratsep <ronald.ratsep@hotmail.com>
2023-06-03 12:15:31 +0300

    Improving entity mapping of Car

--- src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
+++ src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
@@ -1,9 +1,25 @@
 package com.sda.javaremoteee21spring.entity;
 
 public enum Colour {
-    SOUL_RED,
-    RED,
-    BLUE,
-    PINK,
-    GRAY
+    SOUL_RED("Nice red"),
+    RED("Normal red"),
+    BLUE("Just blue"),
+    PINK("Pinky"),
+    GRAY("Just gray");
+
+    private String description;
+
+    public String getDescription(){
+        return description;
+    }
+    Colour(String description) {
+        this.description = description;
+    }
+
+    public static void main(String[] args) {
+        Colour soulRed = SOUL_RED;
+        System.out.println(soulRed.getDescription());
+        System.out.println("name is " + soulRed.name());
+        System.out.println("Ordinal is: " + soulRed.ordinal());
+    }
 }
